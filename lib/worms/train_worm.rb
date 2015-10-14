f8e29e600bda92e06a8a1e21edf1e63290a24afa
ajfaraday <andrewfaraday@hotmail.co.uk>
2015-10-14 22:07:09 +0100

    Added rather entertaining train worm

---
+++ lib/worms/train_worm.rb
@@ -0,0 +1,22 @@
+class TrainWorm < Worm
+
+  def class_initialize
+    until @x_speed != 0 or @y_speed != 0
+      @x_speed = (rand(3) - 1)
+      @y_speed = (rand(3) - 1)
+    end
+  end
+
+  def update_position
+    start_x = @x_position
+    @x_position += @x_speed
+    x_changed = start_x.to_i != @x_position.to_i
+
+    start_y = @y_position
+    @y_position += @y_speed
+    y_changed = start_y.to_i != @y_position.to_i
+
+    @position_changed = true if x_changed or y_changed
+  end
+
+end
