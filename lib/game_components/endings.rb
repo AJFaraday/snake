f2e33c18499aada39e1d56aa26536c0ab22fafb5
ajfaraday <andrewfaraday@hotmail.co.uk>
2015-10-21 19:43:44 +0100

    Actual level progression

--- lib/game_components/endings.rb
+++ lib/game_components/endings.rb
@@ -19,7 +19,9 @@ module GameComponents
     end
 
     def win_ending
-      @ending_text = File.read(
+      @worms = []
+      @ending_count ||= 3
+      @ending_text ||= File.read(
         File.join(
           File.join(
             File.dirname(__FILE__),
@@ -29,7 +31,20 @@ module GameComponents
           )
         )
       )
-      @ended = true
+      if @level['next']
+        until @ending_count <= 0
+          system('clear')
+          puts @ending_text
+          puts "Next level in: #{@ending_count}"
+          @ending_count -= 1
+          sleep 1
+        end
+        @ending_count = nil
+        @ending_text = nil
+        init_level(@level['next'])
+      else
+        @ended = true
+      end
     end
 
     def lose_ending(winner)
